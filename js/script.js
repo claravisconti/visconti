fb77045f3c7dc724011e8254531e0e14f3f0feda
Maria Clara Visconti <claravisconti@hotmail.com>
2021-09-24 12:38:31 -0300

    ejercicioUno

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var numeroUno = parseInt(prompt("Ingrese un número"));
+var numeroDos = parseInt(prompt("Ingrese otro número"));
+
+alert(`${numeroUno} + ${numeroDos} es ${numeroUno + numeroDos}`);
+
+
