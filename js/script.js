5861cea3f3ef2c144239aa124613bded3ed6c2ad
Maria Clara Visconti <claravisconti@hotmail.com>
2021-09-27 20:11:01 -0300

    ejercicio dos

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
-var numeroUno = parseInt(prompt("Ingrese un número"));
-var numeroDos = parseInt(prompt("Ingrese otro número"));
+// var numeroUno = prompt("Ingrese un número");
+// var numeroDos = prompt("Ingrese otro número");
 
-alert(`${numeroUno} + ${numeroDos} es ${numeroUno + numeroDos}`);
+// // alert(`${numeroUno} + ${numeroDos} es ${numeroUno + numeroDos}`);
 
+// if ((numeroUno !== "") && (numeroDos != "")) {
+//     alert(`${numeroUno} + ${numeroDos} es ${(Number(numeroUno) + Number(numeroDos))}`);
+// } else {
+//     alert("Debe ingresar dos números")
+// }
+
+var numeroUno = Number(prompt("Ingrese un número"));
+var numeroDos = Number(prompt("Ingrese otro número"));
+
+if ((numeroUno !== "") && (numeroDos != "")) {
+    alert(`${numeroUno} + ${numeroDos} es ${numeroUno + numeroDos}`);
+} else {
+    alert("Debe existir un valor")
+}
 
